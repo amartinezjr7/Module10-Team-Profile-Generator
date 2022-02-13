4bb57941b7a217da3c3ee7d364f2c5d36d22ed4c
amartinezjr7 <94876426+amartinezjr7@users.noreply.github.com>
2022-02-13 12:25:23 -0800

    first part completed
    manager info completed, just need to write the employee section

--- index.js
+++ index.js
@@ -6,13 +6,78 @@ const promptUser=() =>{
     return inquirer.prompt([
         {
             type:'input',
-            name:'name',
-            message:'What is your name? (require)',
-            validate: nameInput =>{
-                if (nameInput){
+            name:'teamName',
+            message:'What is the name of your Team? (require)',
+            validate: teamNameInput =>{
+                if (teamNameInput){
                     return true;
                 }else{
-                    console.log('please enter your name!');
+                    console.log('please enter your team name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type:'input',
+            name:'managerName',
+            message:'Please Enter the managers name(required)',
+            validate: managerInput=>{
+                if(managerInput){
+                    return true;
+                }else{
+                    console.log("Please Enter the manager's name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type:'input',
+            name:'managerID',
+            message:'Please enter the Managers employee ID (required)',
+            validate: managerID=>{
+                if(managerID){
+                    return true;
+                }else{
+                    console.log('Please Enter the manager ID');
+                    return false;
+                }
+            }
+        },
+        {
+            type:'link',
+            name:'managerEmail',
+            message:'Please enter the managers email (required)',
+            validate: manEmail=>{
+                if(manEmail){
+                    return true;
+                }else{
+                    console.log('Please enter the managers email');
+                    return false;
+                }
+            }
+        },
+        {
+            type:'input',
+            name:'managerGithub',
+            message:'Please enter the managers github handle (required)',
+            validate: manGit =>{
+                if(manGit){
+                    return true;
+                }else{
+                    console.log('manager github required');
+                    return false; 
+                }
+            }
+        },
+        {
+            type:'input',
+            name:'officeNum',
+            message:'Please Enter the office Number (required)',
+            validate:offNum=>{
+                if(offNum){
+                    return true;
+                }else{
+                    console.log('You must enter the office number');
                     return false;
                 }
             }
@@ -21,10 +86,10 @@ const promptUser=() =>{
 };
 
 promptUser()
-    .then(portfolioData =>{
-        console.log(portfolioData);
+    .then(teamData =>{
+        console.log(teamData);
 
-        const pageHTML = generatePage(portfolioData);
+        const pageHTML = generatePage(teamData);
 
         fs.writeFile('index.html', pageHTML, err =>{
             if(err) throw new Error (err);
--- src/page-template.js
+++ src/page-template.js
@@ -9,12 +9,29 @@ module.exports = templateData =>{
     <title>Team Profile Generator</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <header>
-        ${templateData.name}
-    </header>
+  <header>
+  <div class="container align-center">
+    <h1 class="page-title text-light bg-danger py-5 px-5 text-center">${templateData.teamName}</h1>
+  </div>
+</header>
+    <main class="constainer">
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h3 class="card-title bg-secondary text-light text-center">${templateData.managerName}</h3>
+            <h4 class="card-subtitle mb-2 text-light text-center bg-secondary">Manager</h4>
+            <h6>Employee ID: ${templateData.managerID}</h6>
+            <h6>Email:
+            <a href="${templateData.managerEmail}" class="card-link">${templateData.managerEmail}</a></h6>
+            <h6>Github:
+            <a href="https://www.github.com/${templateData.managerGithub}" class="card-link">${templateData.managerGithub}</a><h6>
+            <h6>Office Number:<a>${templateData.officeNum}</a></h6>
+         </div>
+        </div>
+    </main>
 
   </body>
   </html>`;
